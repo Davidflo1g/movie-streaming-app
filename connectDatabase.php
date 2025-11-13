422701d163d402a0b5298167c93e82ef84c46140
mhtran33 <142823293+mhtran33@users.noreply.github.com>
2025-11-13 16:14:19 -0500

    Update connectDatabase.php

--- connectDatabase.php
+++ connectDatabase.php
@@ -1,10 +1,9 @@
 <?php
-$servername = "localhost";
-$username = "guest";
-$password = "ggcITEC4450@";
-$dbname = "moviedb";
-					
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Use config.php for database connection
+require_once "config.php";
+
+// Create connection using config.php variables
+$conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
